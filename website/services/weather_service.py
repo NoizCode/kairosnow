3dbfd244c379f82d0de128b35fea5166f88df0e0
NoizCode <omgmycanonminion@gmail.com>
2025-03-13 11:02:52 +0200

    Create tables for all cities

--- website/services/weather_service.py
+++ website/services/weather_service.py
@@ -11,54 +11,54 @@ import requests
 import json
 
 API_KEY = "c97a89a1a175f935e3f864807e4643f4"
-city = "Agrinio"
+cities = ["Agrinio", "Athens", "Patra", "Kalamata", "Tripoli", "Thessaloniki", "Karpenisi", "Chalcis", "Volos"]
 
-response = requests.get(f"http://api.openweathermap.org/data/2.5/forecast?q={city}&appid={API_KEY}")
+for current_city in cities:
+    response = requests.get(f"http://api.openweathermap.org/data/2.5/forecast?q={current_city}&appid={API_KEY}")
 
-# PRETTY PRINT JSON
-json_data = json.dumps(response.json(), indent=4) 
-#print(json_data)
+    ### PRETTY PRINT JSON ###
+    #json_data = json.dumps(response.json(), indent=4) 
+    #print(json_data)
 
-weather_data = {}
-data = [] # Temporarily store "main" and "weather" data
+    weather_data = {}
+    data = [] # Temporarily store "main" and "weather" data
 
-for item in response.json()["list"]:
-    data.extend([item["main"], item["weather"], item["wind"]])
-    weather_data[item["dt_txt"]] = data
-    data = []
+    for item in response.json()["list"]:    
+        data.extend([item["main"], item["weather"], item["wind"]])
+        weather_data[item["dt_txt"]] = data
+        data = []
 
-# Print data for debugging
-#for key in weather_data:
-#    print("\n")
-#    print(key,":", weather_data[key])
+    ### Print data for debugging ###
+    #for key in weather_data:
+    #    print("\n")
+    #    print(key,":", weather_data[key])
 
-app = create_app()
+    app = create_app()
 
-with app.app_context():
-    city_add = City(name=city)
-    db.session.add(city_add)
-    db.session.commit()
+    with app.app_context():
+        city_add = City(name=current_city)
+        db.session.add(city_add)
+        db.session.commit()
 
-    for key,value in weather_data.items():
-        timestamp = datetime.strptime(key, "%Y-%m-%d %H:%M:%S")
+        for key,value in weather_data.items():
+            timestamp = datetime.strptime(key, "%Y-%m-%d %H:%M:%S")
 
-        temp_data = value[0]
-        main_weath = value[1]
-        wind_data = value[2]
+            temp_data = value[0]
+            main_weath = value[1]
+            wind_data = value[2]
 
-        new_timestamp = Timestamp(
-            city_id  = city_add.id,
-            time = timestamp,
-            temp = temp_data["temp"],
-            feels_like = temp_data["feels_like"],
-            temp_min = temp_data["temp_min"],
-            temp_max = temp_data["temp_max"],
-            humidity = temp_data["humidity"],
-            main_weath = main_weath[0]["main"],
-            main_desc = main_weath[0]["description"],
-            wind_speed = wind_data["speed"]
-        )
-        db.session.add(new_timestamp)
+            new_timestamp = Timestamp(
+                city_id  = city_add.id,
+                time = timestamp,
+                temp = temp_data["temp"],
+                feels_like = temp_data["feels_like"],
+                temp_min = temp_data["temp_min"],
+                temp_max = temp_data["temp_max"],
+                humidity = temp_data["humidity"],
+                main_weath = main_weath[0]["main"],
+                main_desc = main_weath[0]["description"],
+                wind_speed = wind_data["speed"]
+            )
+            db.session.add(new_timestamp)
 
-    db.session.commit()
-    print(Timestamp.query.options(joinedload(Timestamp.city)).all())
+        db.session.commit()
