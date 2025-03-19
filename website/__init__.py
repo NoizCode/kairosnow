6a7e36f927cc56a56f447cbcbca323f0501baff5
NoizCode <omgmycanonminion@gmail.com>
2025-03-19 15:33:39 +0200

    update interval

--- website/__init__.py
+++ website/__init__.py
@@ -23,7 +23,7 @@ def create_app():
 
     # Scheduler Setup
     scheduler = BackgroundScheduler()
-    scheduler.add_job(fetch_and_store_data, 'interval', minutes=60)
+    scheduler.add_job(fetch_and_store_data, 'interval', minutes=10)
     scheduler.start()
 
     return app
